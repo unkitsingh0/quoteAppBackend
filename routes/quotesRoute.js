3f34f79c9526975344e79b369bad4031e880fa5a
Ankit Singh <52825138+unkitsingh0@users.noreply.github.com>
2023-07-31 10:42:02 +0530

    dev purpose

--- routes/quotesRoute.js
+++ routes/quotesRoute.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Email from "../models/userModel.js";
 import mailServer from "../connections/mailServices.js";
 import SendEmail from "../models/sendEmailModel.js";
+import telegram from "node-telegram-bot-api";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -23,7 +24,7 @@ let sendDailyQuotesTOUser = async () => {
     });
     let getSendEmail = await SendEmail.findOne({});
     let send = getSendEmail.send;
-    if (new Date().getHours() == 8 && send == true) {
+    if (new Date().getHours() == 22 && send == true) {
       console.log(
         "sending email to all emails" +
           new Date().toLocaleDateString() +
@@ -43,7 +44,7 @@ let sendDailyQuotesTOUser = async () => {
     } else {
       console.log("not sending mail");
     }
-    if (new Date().getHours() > 8) {
+    if (new Date().getHours() > 22) {
       if (send == false) {
         console.log(send);
         let updateSendEmail = await SendEmail.updateOne(
@@ -53,9 +54,31 @@ let sendDailyQuotesTOUser = async () => {
         console.log("updating sendEmail");
       }
     }
-  }, 3600000);
+  }, 10000);
 };
 //3600000 This is milisecond
 //This milisecond is equal to 60 min
 sendDailyQuotesTOUser();
+
+// Writing some telegram bot code here
+// let token = "5544617273:AAFgHA1AvYvWN4gTqkc9G7O2YWVA7YikFDE";
+// let bot = new telegram(token, { polling: true });
+
+// bot.on("message", async (message) => {
+//   let chat_id = message.from.id;
+//   console.log(message);
+//   let { data } = await axios.get("https://zenquotes.io/api/random");
+//   let quote = data;
+//   console.log(quote);
+//   // bot.sendMessage(['5899998012','610269940'], `${quote[0].q}\n${quote[0].a}`);
+//   let user = ["610269940", "5899998012"];
+//   setInterval(() => {
+//     user.map((user) => {
+//       bot.sendMessage(user, `baki sab thik he `);
+//     });
+//   }, 10000);
+//   console.log("Bot is active");
+// });
 export default router;
+//610269940
+//5899998012
--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 //Connecting to database
-connecToDatabase(process.env.Database_uri);
+connecToDatabase(process.env.Database_uri_atlas);
 
 //Routes
 app.use("/api/quotes", quotesRouter);
